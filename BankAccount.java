431c36610d8c0dc386865d0d8302bd69f511065c
will-o-the-wisp <kli18@stuy.edu>
2018-10-03 20:48:05 -0400

    finished debugging BankAccount

--- BankAccount.java
+++ BankAccount.java
@@ -2,7 +2,7 @@ public class BankAccount{
   private double balance;
   private int accountID;
   private String password;
-  public BankAccount(double balance, accountID int, String password){
+  public BankAccount(double balance, int accountID, String password){
     this.balance = balance;
     this.accountID = accountID;
     this.password = password;
